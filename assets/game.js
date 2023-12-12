0a969f4c464ea7de8a3e80d11fccdf37c0e31d29
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2023-12-12 15:04:03 +0000

    #

--- assets/game.js
+++ assets/game.js
@@ -8,67 +8,67 @@ const novoJogoBtn = document.querySelector(".play-again");
 let palavraSelecionada, letrasCorretas, contadorDeErros;
 const maximoDeErros = 6;
 const resetGame = () => {
- // Resetando todas as variaveis
- letrasCorretas = [];
- contadorDeErros = 0;
- hangmanImage.src = `assets/images/hangman-${contadorDeErros}.svg`;
- guessesText.innerText = `${contadorDeErros} / ${maximoDeErros}`;
- keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
- wordDisplay.innerHTML = palavraSelecionada.split("").map(() => `<li class="letter"></li>`).join("");
- gameModal.classList.remove("show");
+    // Resetando todas as variaveis
+    letrasCorretas = [];
+    contadorDeErros = 0;
+    hangmanImage.src = `assets/images/hangman-${contadorDeErros}.svg`;
+    guessesText.innerText = `${contadorDeErros} / ${maximoDeErros}`;
+    keyboardDiv.querySelectorAll("button").forEach(btn => btn.disabled = false);
+    wordDisplay.innerHTML = palavraSelecionada.split("").map(() => `<li class="letter"></li>`).join("");
+    gameModal.classList.remove("show");
 }
 
 // Pegando uma palavra e uma dica aleatoriamente
 const getRandomWord = () => {
- const { palavra, dica } = !localStorage.getItem('personalizado') ? listaPalavras[Math.floor(Math.random() * listaPalavras.length)] : JSON.parse(localStorage.getItem('personalizado'));
- localStorage.clear();
- palavraSelecionada = palavra;
- console.log(palavra);
- document.querySelector(".hint-text b").innerText = dica;
- resetGame();
+    const { palavra, dica } = !sessionStorage.getItem('personalizado') ? listaPalavras[Math.floor(Math.random() * listaPalavras.length)] : JSON.parse(sessionStorage.getItem('personalizado'));
+    sessionStorage.clear();
+    palavraSelecionada = palavra;
+    console.log(palavra);
+    document.querySelector(".hint-text b").innerText = dica;
+    resetGame();
 }
 
 const fimDeJogo = (ganhou) => {
- // Mostrando o modal de fim de jogo com o conteúdo dinâmico de acondo com a vitória ou derrota
- setTimeout(() => {
-  const modalText = ganhou ? `Você achou a palavra: ` : `A palavra correta era: `
-  gameModal.querySelector("img").src = `assets/images/${ganhou ? 'victory' : 'lost'}.gif`;
-  gameModal.querySelector("h4").innerText = `${ganhou ? 'Parabéns!!!' : 'Você perdeu ;-; !!!'}`;
-  gameModal.querySelector("p").innerHTML = `${modalText}<b>${palavraSelecionada}</b>`;
-  gameModal.classList.add("show");
- }, 300)
+    // Mostrando o modal de fim de jogo com o conteúdo dinâmico de acondo com a vitória ou derrota
+    setTimeout(() => {
+        const modalText = ganhou ? `Você achou a palavra: ` : `A palavra correta era: `
+        gameModal.querySelector("img").src = `assets/images/${ganhou ? 'victory' : 'lost'}.gif`;
+        gameModal.querySelector("h4").innerText = `${ganhou ? 'Parabéns!!!' : 'Você perdeu ;-; !!!'}`;
+        gameModal.querySelector("p").innerHTML = `${modalText}<b>${palavraSelecionada}</b>`;
+        gameModal.classList.add("show");
+    }, 300)
 }
 
 const inicarJogo = (button, clickedLetter) => {
- if (palavraSelecionada.includes(clickedLetter)) {
-  // Exibindo a letra corretamente selecionadas na tela
-  [...palavraSelecionada].forEach((letter, index) => {
-   if (letter === clickedLetter) {
-    letrasCorretas.push(letter)
-    wordDisplay.querySelectorAll("li")[index].innerText = letter;
-    wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
-   }
-  })
- } else {
-  // Atualizando a imagem quando houver erro
-  contadorDeErros = (contadorDeErros == 6) ? 6 : contadorDeErros + 1;
-  hangmanImage.src = `assets/images/hangman-${contadorDeErros}.svg`;
-  guessesText.innerText = `${contadorDeErros} / ${maximoDeErros}`;
- }
+    if (palavraSelecionada.includes(clickedLetter)) {
+        // Exibindo a letra corretamente selecionadas na tela
+        [...palavraSelecionada].forEach((letter, index) => {
+            if (letter === clickedLetter) {
+                letrasCorretas.push(letter)
+                wordDisplay.querySelectorAll("li")[index].innerText = letter;
+                wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+            }
+        })
+    } else {
+        // Atualizando a imagem quando houver erro
+        contadorDeErros = (contadorDeErros == 6) ? 6 : contadorDeErros + 1;
+        hangmanImage.src = `assets/images/hangman-${contadorDeErros}.svg`;
+        guessesText.innerText = `${contadorDeErros} / ${maximoDeErros}`;
+    }
 
- button.disabled = true;
+    button.disabled = true;
 
- // Verificando sucesso é fracasso do jogador
- if (contadorDeErros === maximoDeErros) return fimDeJogo(false);
- if (letrasCorretas.length === palavraSelecionada.length) return fimDeJogo(true);
+    // Verificando sucesso é fracasso do jogador
+    if (contadorDeErros === maximoDeErros) return fimDeJogo(false);
+    if (letrasCorretas.length === palavraSelecionada.length) return fimDeJogo(true);
 }
 
 // Criando os botões do teclado e adicionando o evento de click
 for (let i = 0; i < 26; i++) {
- const button = document.createElement("button");
- button.innerText = String.fromCharCode(97 + i);
- keyboardDiv.appendChild(button);
- button.addEventListener("click", e => inicarJogo(e.target, String.fromCharCode(97 + i)));
+    const button = document.createElement("button");
+    button.innerText = String.fromCharCode(97 + i);
+    keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => inicarJogo(e.target, String.fromCharCode(97 + i)));
 }
 
 getRandomWord();
--- assets/personalizado.js
+++ assets/personalizado.js
@@ -2,43 +2,43 @@ const alertMessage = document.querySelector(".alert-message");
 const alertTitle = document.querySelector(".alert-title");
 
 function verificarDados() {
- let palavra = document.querySelector(".palavra").value;
- let dica = document.querySelector(".dica").value;
- let mensagem;
+    let palavra = document.querySelector(".palavra").value;
+    let dica = document.querySelector(".dica").value;
+    let mensagem;
 
- if (!palavra || !dica) {
-  mensagem = 'Digite uma palavra e dica!!'
- } else if (palavra.includes(' ') || palavra.includes('-')) {
-  mensagem = 'A palavra não pode conter espaços nem hífens'
- } else if (contemCaracteresEspeciais(palavra)) {
-  mensagem = 'A palavra não pode conter caracteres especiais'
- } else {
-  localStorage.setItem('personalizado', JSON.stringify({
-   palavra: palavra,
-   dica: dica
-  }));
+    if (!palavra || !dica) {
+        mensagem = 'Digite uma palavra e dica!!'
+    } else if (palavra.includes(' ') || palavra.includes('-')) {
+        mensagem = 'A palavra não pode conter espaços nem hífens'
+    } else if (contemCaracteresEspeciais(palavra)) {
+        mensagem = 'A palavra não pode conter caracteres especiais'
+    } else {
+        sessionStorage.setItem('personalizado', JSON.stringify({
+            palavra: palavra,
+            dica: dica
+        }));
 
-  setTimeout(function () {
-   window.location.href = "./game.html";
-  }, 100);
-  return;
- }
+        setTimeout(function () {
+            window.location.href = "./game.html";
+        }, 100);
+        return;
+    }
 
- alertTitle.textContent = mensagem;
- exibirMensagem(true);
- setTimeout(() => {
-  exibirMensagem(false);
- }, 2000)
+    alertTitle.textContent = mensagem;
+    Mensagem(true);
+    setTimeout(() => {
+        Mensagem(false);
+    }, 2000)
 }
 
 function contemCaracteresEspeciais(s) {
- return /[^a-z\s]/g.test(s);
+    return /[^a-z\s]/g.test(s);
 }
 
-function exibirMensagem(bool) {
- if (bool) {
-  alertMessage.classList.add('show');
-  return;
- }
- alertMessage.classList.remove('show');
+function Mensagem(bool) {
+    if (bool) {
+        alertMessage.classList.add('show');
+        return;
+    }
+    alertMessage.classList.remove('show');
 }
