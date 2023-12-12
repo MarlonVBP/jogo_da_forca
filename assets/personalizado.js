69bea1fed749bce9fe9431d41ba11ae786413b82
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2023-12-12 11:06:04 -0300

    Update personalizado.js

--- assets/personalizado.js
+++ assets/personalizado.js
@@ -1,17 +1,44 @@
+const alertMessage = document.querySelector(".alert-message");
+const alertTitle = document.querySelector(".alert-title");
+
 function verificarDados() {
  let palavra = document.querySelector(".palavra").value;
  let dica = document.querySelector(".dica").value;
+ let mensagem;
 
- if ((palavra && dica) && !contemCaracteresEspeciais(palavra) && !palavra.includes(' ') && !palavra.includes('-')) {
+ if (!palavra || !dica) {
+  mensagem = 'Digite uma palavra e dica!!'
+ } else if (palavra.includes(' ') || palavra.includes('-')) {
+  mensagem = 'A palavra não pode conter espaços nem hífens'
+ } else if (contemCaracteresEspeciais(palavra)) {
+  mensagem = 'A palavra não pode conter caracteres especiais'
+ } else {
   localStorage.setItem('personalizado', JSON.stringify({
    palavra: palavra,
    dica: dica
   }));
+
   setTimeout(function () {
    window.location.href = "./game.html";
   }, 100);
+  return;
  }
+
+ alertTitle.textContent = mensagem;
+ exibirMensagem(true);
+ setTimeout(() => {
+  exibirMensagem(false);
+ }, 2000)
 }
+
 function contemCaracteresEspeciais(s) {
  return /[^a-z\s]/g.test(s);
+}
+
+function exibirMensagem(bool) {
+ if (bool) {
+  alertMessage.classList.add('show');
+  return;
+ }
+ alertMessage.classList.remove('show');
 }
