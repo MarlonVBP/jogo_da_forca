b927e680f9bbf0652d31be71b00bafe9767a9898
Marlon Passos <101027484+MarlonVBP@users.noreply.github.com>
2023-12-18 19:42:52 +0000

    Update wordList

--- assets/word-list.js
+++ assets/word-list.js
@@ -1,218 +1,218 @@
-const listaPalavras = [
+const wordList = [
  {
-  palavra: "chocolate",
-  dica: "Um doce feito a partir de grãos de cacau."
+  word: "chocolate",
+  hint: "Um doce feito a partir de grãos de cacau."
  },
  {
-  palavra: "borboleta",
-  dica: "Um inseto com asas coloridas e um corpo esguio."
+  word: "borboleta",
+  hint: "Um inseto com asas coloridas e um corpo esguio."
  },
  {
-  palavra: "historia",
-  dica: "O estudo de eventos passados e da civilização humana."
+  word: "historia",
+  hint: "O estudo de eventos passados e da civilização humana."
  },
  {
-  palavra: "pizza",
-  dica: "Um prato salgado composto por uma base redonda e achatada com coberturas."
+  word: "pizza",
+  hint: "Um prato salgado composto por uma base redonda e achatada com coberturas."
  },
  {
-  palavra: "jazz",
-  dica: "Um gênero de música caracterizado por improvisação e sincopação."
+  word: "jazz",
+  hint: "Um gênero de música caracterizado por improvisação e sincopação."
  },
  {
-  palavra: "camera",
-  dica: "Um dispositivo usado para capturar e gravar imagens ou vídeos."
+  word: "camera",
+  hint: "Um dispositivo usado para capturar e gravar imagens ou vídeos."
  },
  {
-  palavra: "diamante",
-  dica: "Uma pedra preciosa conhecida por sua brilhância e dureza."
+  word: "diamante",
+  hint: "Uma pedra preciosa conhecida por sua brilhância e dureza."
  },
  {
-  palavra: "aventura",
-  dica: "Uma experiência emocionante ou ousada."
+  word: "aventura",
+  hint: "Uma experiência emocionante ou ousada."
  },
  {
-  palavra: "ciencia",
-  dica: "O estudo sistemático da estrutura e comportamento do mundo físico e natural."
+  word: "ciencia",
+  hint: "O estudo sistemático da estrutura e comportamento do mundo físico e natural."
  },
  {
-  palavra: "bicicleta",
-  dica: "Um veículo movido a energia humana com duas rodas."
+  word: "bicicleta",
+  hint: "Um veículo movido a energia humana com duas rodas."
  },
  {
-  palavra: "cafe",
-  dica: "Uma bebida cafeinada popular feita a partir de grãos de café torrados."
+  word: "cafe",
+  hint: "Uma bebida cafeinada popular feita a partir de grãos de café torrados."
  },
  {
-  palavra: "danca",
-  dica: "Um movimento rítmico do corpo frequentemente realizado com música."
+  word: "danca",
+  hint: "Um movimento rítmico do corpo frequentemente realizado com música."
  },
  {
-  palavra: "galaxia",
-  dica: "Um vasto sistema de estrelas, gás e poeira mantido unido pela gravidade."
+  word: "galaxia",
+  hint: "Um vasto sistema de estrelas, gás e poeira mantido unido pela gravidade."
  },
  {
-  palavra: "orquestra",
-  dica: "Um grande grupo de músicos tocando vários instrumentos."
+  word: "orquestra",
+  hint: "Um grande grupo de músicos tocando vários instrumentos."
  },
  {
-  palavra: "vulcao",
-  dica: "Uma montanha ou colina com uma abertura pela qual lava, fragmentos de rocha, vapor quente e gás são ejetados."
+  word: "vulcao",
+  hint: "Uma montanha ou colina com uma abertura pela qual lava, fragmentos de rocha, vapor quente e gás são ejetados."
  },
  {
-  palavra: "romance",
-  dica: "Uma obra longa de ficção, tipicamente com uma trama e personagens complexos."
+  word: "romance",
+  hint: "Uma obra longa de ficção, tipicamente com uma trama e personagens complexos."
  },
  {
-  palavra: "escultura",
-  dica: "Uma forma de arte tridimensional criada moldando ou combinando materiais."
+  word: "escultura",
+  hint: "Uma forma de arte tridimensional criada moldando ou combinando materiais."
  },
  {
-  palavra: "sinfonia",
-  dica: "Uma composição musical longa para uma orquestra completa, frequentemente em vários movimentos."
+  word: "sinfonia",
+  hint: "Uma composição musical longa para uma orquestra completa, frequentemente em vários movimentos."
  },
  {
-  palavra: "arquitetura",
-  dica: "A arte e ciência de projetar e construir edifícios."
+  word: "arquitetura",
+  hint: "A arte e ciência de projetar e construir edifícios."
  },
  {
-  palavra: "ballet",
-  dica: "Uma forma de dança clássica caracterizada por movimentos precisos e graciosos."
+  word: "ballet",
+  hint: "Uma forma de dança clássica caracterizada por movimentos precisos e graciosos."
  },
  {
-  palavra: "astronauta",
-  dica: "Uma pessoa treinada para viajar e trabalhar no espaço."
+  word: "astronauta",
+  hint: "Uma pessoa treinada para viajar e trabalhar no espaço."
  },
  {
-  palavra: "cachoeira",
-  dica: "Uma cascata de água caindo de uma altura."
+  word: "cachoeira",
+  hint: "Uma cascata de água caindo de uma altura."
  },
  {
-  palavra: "tecnologia",
-  dica: "A aplicação do conhecimento científico para fins práticos."
+  word: "tecnologia",
+  hint: "A aplicação do conhecimento científico para fins práticos."
  },
  {
-  palavra: "universo",
-  dica: "Todo o material existente, espaço e tempo como um todo."
+  word: "universo",
+  hint: "Todo o material existente, espaço e tempo como um todo."
  },
  {
-  palavra: "piano",
-  dica: "Um instrumento musical tocado pressionando teclas que fazem martelos baterem nas cordas."
+  word: "piano",
+  hint: "Um instrumento musical tocado pressionando teclas que fazem martelos baterem nas cordas."
  },
  {
-  palavra: "ferias",
-  dica: "Um período de tempo dedicado ao prazer, descanso ou relaxamento."
+  word: "ferias",
+  hint: "Um período de tempo dehintdo ao prazer, descanso ou relaxamento."
  },
  {
-  palavra: "teatro",
-  dica: "Um edifício ou área ao ar livre onde peças teatrais, filmes ou outras performances são encenados."
+  word: "teatro",
+  hint: "Um edifício ou área ao ar livre onde peças teatrais, filmes ou outras performances são encenados."
  },
  {
-  palavra: "telefone",
-  dica: "Um dispositivo usado para transmitir som a longas distâncias."
+  word: "telefone",
+  hint: "Um dispositivo usado para transmitir som a longas distâncias."
  },
  {
-  palavra: "idioma",
-  dica: "Um sistema de comunicação composto por palavras, gestos e sintaxe."
+  word: "idioma",
+  hint: "Um sistema de comunicação composto por words, gestos e sintaxe."
  },
  {
-  palavra: "deserto",
-  dica: "Uma terra estéril ou árida com pouca ou nenhuma precipitação."
+  word: "deserto",
+  hint: "Uma terra estéril ou árida com pouca ou nenhuma precipitação."
  },
  {
-  palavra: "girassol",
-  dica: "Uma planta alta com uma grande cabeça de flor amarela."
+  word: "girassol",
+  hint: "Uma planta alta com uma grande cabeça de flor amarela."
  },
  {
-  palavra: "fantasia",
-  dica: "Um gênero de ficção imaginativa envolvendo magia e elementos sobrenaturais."
+  word: "fantasia",
+  hint: "Um gênero de ficção imaginativa envolvendo magia e elementos sobrenaturais."
  },
  {
-  palavra: "telescopio",
-  dica: "Um instrumento óptico usado para ver objetos distantes no espaço."
+  word: "telescopio",
+  hint: "Um instrumento óptico usado para ver objetos distantes no espaço."
  },
  {
-  palavra: "brisa",
-  dica: "Um vento suave."
+  word: "brisa",
+  hint: "Um vento suave."
  },
  {
-  palavra: "oasis",
-  dica: "Um local fértil em um deserto onde água é encontrada."
+  word: "oasis",
+  hint: "Um local fértil em um deserto onde água é encontrada."
  },
  {
-  palavra: "fotografia",
-  dica: "A arte, processo ou prática de criar imagens registrando a luz ou outra radiação eletromagnética."
+  word: "fotografia",
+  hint: "A arte, processo ou prática de criar imagens registrando a luz ou outra radiação eletromagnética."
  },
  {
-  palavra: "safari",
-  dica: "Uma expedição ou jornada, tipicamente para observar a vida selvagem em seu habitat natural."
+  word: "safari",
+  hint: "Uma expedição ou jornada, tipicamente para observar a vida selvagem em seu habitat natural."
  },
  {
-  palavra: "planeta",
-  dica: "Um corpo celeste que orbita uma estrela e não produz luz própria."
+  word: "planeta",
+  hint: "Um corpo celeste que orbita uma estrela e não produz luz própria."
  },
  {
-  palavra: "rio",
-  dica: "Um grande curso natural de água fluindo em um canal para o mar, um lago ou outro curso de água semelhante."
+  word: "rio",
+  hint: "Um grande curso natural de água fluindo em um canal para o mar, um lago ou outro curso de água semelhante."
  },
  {
-  palavra: "tropical",
-  dica: "Relativo ou situado na região entre o Trópico de Câncer e o Trópico de Capricórnio."
+  word: "tropical",
+  hint: "Relativo ou situado na região entre o Trópico de Câncer e o Trópico de Capricórnio."
  },
  {
-  palavra: "misterioso",
-  dica: "Difícil ou impossível de entender, explicar ou identificar."
+  word: "misterioso",
+  hint: "Difícil ou impossível de entender, explicar ou identificar."
  },
  {
-  palavra: "enigma",
-  dica: "Algo misterioso, intrigante ou difícil de entender."
+  word: "enigma",
+  hint: "Algo misterioso, intrigante ou difícil de entender."
  },
  {
-  palavra: "paradoxo",
-  dica: "Uma afirmação ou situação que se contradiz ou desafia a intuição."
+  word: "paradoxo",
+  hint: "Uma afirmação ou situação que se contradiz ou desafia a intuição."
  },
  {
-  palavra: "sussurro",
-  dica: "Falar muito suavemente ou silenciosamente, frequentemente de maneira secreta."
+  word: "sussurro",
+  hint: "Falar muito suavemente ou silenciosamente, frequentemente de maneira secreta."
  },
  {
-  palavra: "sombra",
-  dica: "Uma área ou forma escura produzida por um objeto bloqueando a luz."
+  word: "sombra",
+  hint: "Uma área ou forma escura produzida por um objeto bloqueando a luz."
  },
  {
-  palavra: "segredo",
-  dica: "Algo mantido escondido ou desconhecido para os outros."
+  word: "segredo",
+  hint: "Algo mantido escondido ou desconhecido para os outros."
  },
  {
-  palavra: "curiosidade",
-  dica: "Um forte desejo de saber ou aprender algo."
+  word: "curiosidade",
+  hint: "Um forte desejo de saber ou aprender algo."
  },
  {
-  palavra: "imprevisivel",
-  dica: "Não capaz de ser previsto ou conhecido antecipadamente; incerto."
+  word: "imprevisivel",
+  hint: "Não capaz de ser previsto ou conhecido antecipadamente; incerto."
  },
  {
-  palavra: "ofuscar",
-  dica: "Confundir ou deixar alguém perplexo; tornar algo obscuro ou difícil de entender."
+  word: "ofuscar",
+  hint: "Confundir ou deixar alguém perplexo; tornar algo obscuro ou difícil de entender."
  },
  {
-  palavra: "revelar",
-  dica: "Tornar conhecido ou revelar algo previamente secreto ou desconhecido."
+  word: "revelar",
+  hint: "Tornar conhecido ou revelar algo previamente secreto ou desconhecido."
  },
  {
-  palavra: "ilusao",
-  dica: "Uma percepção ou crença falsa; uma aparência ou impressão enganosa."
+  word: "ilusao",
+  hint: "Uma percepção ou crença falsa; uma aparência ou impressão enganosa."
  },
  {
-  palavra: "vibrante",
-  dica: "Cheio de energia, brilho e vida."
+  word: "vibrante",
+  hint: "Cheio de energia, brilho e vida."
  },
  {
-  palavra: "nostalgia",
-  dica: "Uma saudade sentimental ou afeto saudoso pelo passado."
+  word: "nostalgia",
+  hint: "Uma saudade sentimental ou afeto saudoso pelo passado."
  },
  {
-  palavra: "brilhante",
-  dica: "Excepcionalmente inteligente, talentoso ou impressionante."
+  word: "brilhante",
+  hint: "Excepcionalmente inteligente, talentoso ou impressionante."
  },
 ];
